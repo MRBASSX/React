a7a220f8e2d2c9f9dae62a7f72021b90ce7853ac
MRBASSX <abassiddrisu9222@gmail.com>
2024-07-06 13:01:42 +0100

    I am committing a project

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import logo from './logo.svg';
+import './App.css';
+import './App.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+
+// or less ideally
+// import { Button } from 'react-bootstrap';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+       
+        <div>
+  <Button>
+    Button as link
+  </Button>
+  <Button>
+    Button as link
+  </Button>
+  </div>
+
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
