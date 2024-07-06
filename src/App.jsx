57ecda6160febcf0b0f90555c61861ef0422f810
mrbassx <abassiddrisu9222@gmail.com>
2024-07-06 22:15:02 +0100

    New Day

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
+import Home from './component/Home'
 import logo from './logo.svg';
 import './App.css';
 import './App.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button';
+import {
+	BrowserRouter as Router,
+	Routes,
+	Route,
+} from "react-router-dom";
 
 // or less ideally
 // import { Button } from 'react-bootstrap';
@@ -10,6 +16,14 @@ import Button from 'react-bootstrap/Button';
 function App() {
   return (
     <div className="App">
+      <Router>
+        <Routes>
+          <Route path='/' element={< Home/>}></Route>
+          <Route path='/' element={< Che/>}></Route>
+          <Route path='/' element={< Home/>}></Route>
+          <Route path='/' element={< Home/>}></Route>
+        </Routes>
+      </Router>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
