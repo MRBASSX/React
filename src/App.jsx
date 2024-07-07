689b0a216bfc6d9692b22dea7323dae92c1460fe
mrbassx <abassiddrisu9222@gmail.com>
2024-07-07 21:25:02 +0100

    App.jsx change

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
 
-import logo from './logo.svg';
 import './App.css';
 import './App.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Button from 'react-bootstrap/Button';
 import Nabars from './component/Nabars';
 
 // or less ideally
